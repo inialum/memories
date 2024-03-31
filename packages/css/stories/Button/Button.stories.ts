c766a6e8430e006d25b481af585e1311e8b5c152
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-31 16:32:04 +0900

    chore: Configure stories for Button component

---
+++ packages/css/stories/Button/Button.stories.ts
@@ -0,0 +1,91 @@
+import { fn } from '@storybook/test'
+
+import { type ButtonProps, createButton } from './Button'
+
+import type { ArgTypes, Meta, StoryObj } from '@storybook/html'
+
+const meta = {
+  title: 'Button',
+  tags: ['autodocs'],
+  render: ({ label, ...args }) => {
+    return createButton({ label, ...args })
+  },
+  argTypes: {
+    label: { control: 'text' },
+    colorTheme: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'white'],
+    },
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['small', 'medium', 'large'],
+    },
+    styleType: {
+      control: {
+        type: 'select',
+      },
+      options: ['filled', 'outlined'],
+    },
+    radius: {
+      control: {
+        type: 'select',
+      },
+      options: ['none', 'rounded', 'more-rounded'],
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    onClick: {
+      action: 'onClick',
+    },
+  } satisfies Required<ArgTypes<ButtonProps>>,
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<ButtonProps>
+
+export default meta
+
+type Story = StoryObj<ButtonProps>
+
+export const Default: Story = {
+  args: {
+    label: '入会する',
+    colorTheme: 'primary',
+    size: 'medium',
+    styleType: 'filled',
+    radius: 'none',
+    disabled: false,
+  },
+}
+
+export const Outlined: Story = {
+  args: {
+    ...Default.args,
+    styleType: 'outlined',
+  },
+}
+
+export const Rounded: Story = {
+  args: {
+    ...Default.args,
+    radius: 'rounded',
+  },
+}
+
+export const MoreRounded: Story = {
+  args: {
+    ...Default.args,
+    radius: 'more-rounded',
+    label: 'more...',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+}
---
+++ packages/css/stories/Button/Button.ts
@@ -0,0 +1,50 @@
+import clsx from 'clsx'
+
+export enum ButtonSize {
+  small = 'small',
+  medium = 'medium',
+  large = 'large',
+}
+
+export type ButtonProps = {
+  colorTheme?: 'primary' | 'secondary' | 'white'
+  size?: 'small' | 'medium' | 'large' | 'fullWidth'
+  styleType?: 'filled' | 'outlined'
+  radius?: 'none' | 'rounded' | 'more-rounded'
+  disabled?: boolean
+  label?: string
+  onClick: () => void
+}
+
+export const createButton = ({
+  colorTheme = 'primary',
+  size = 'medium',
+  styleType = 'filled',
+  radius = 'none',
+  disabled = false,
+  label = '',
+  onClick,
+}: ButtonProps) => {
+  const btn = document.createElement('button')
+  btn.type = 'button'
+  btn.innerText = label
+  btn.addEventListener('click', onClick)
+
+  btn.className = clsx(
+    'Button',
+    colorTheme === 'primary' && 'Button--primary',
+    colorTheme === 'secondary' && 'Button--secondary',
+    colorTheme === 'white' && 'Button--white',
+    size === 'small' && 'Button--small',
+    size === 'medium' && 'Button--medium',
+    size === 'large' && 'Button--large',
+    size === 'fullWidth' && 'Button--fullWidth',
+    styleType === 'filled' && 'Button--filled',
+    styleType === 'outlined' && 'Button--outlined',
+    radius === 'rounded' && 'Button--rounded',
+    radius === 'more-rounded' && 'Button--more-rounded',
+    disabled && 'Button--disabled',
+  )
+
+  return btn
+}
