2fd6191640aab7a03bb45cbc1e200255a0559838
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-31 22:18:26 +0900

    feat: Create ButtonLink component

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,13 +1,13 @@
 import plugin from 'tailwindcss/plugin'
 
-import { Button } from '@/components'
+import { Button, ButtonLink } from '@/components'
 import { tailwindConfig } from '@/tailwind/config'
 import { type ConfigOptions } from '@/types'
 
 const memories = plugin.withOptions<ConfigOptions>(
   (options) => {
     return ({ addComponents }) => {
-      addComponents([Button])
+      addComponents([Button, ButtonLink])
 
       if (options?.base !== false) {
         // TODO: Add base styles here
