afc917bfb158f449d0485f6b233dab8134a30212
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-31 23:25:15 +0900

    chore: Formatting

--- packages/css/.storybook/main.ts
+++ packages/css/.storybook/main.ts
@@ -3,7 +3,10 @@ import { resolve } from 'node:path'
 import type { StorybookConfig } from '@storybook/html-vite'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
