1eb070dc417605ca6b0ab007baf5fef40472762c
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-04-07 04:54:55 +0900

    feat: Setup project for memories-react

---
+++ packages/react/README.md
@@ -0,0 +1,3 @@
+# @inialum/memories-react
+
+React component library for the Memories - INIALUM Design System.
---
+++ packages/react/tailwind.config.ts
@@ -0,0 +1,9 @@
+import { memories } from '@inialum/memories-css'
+import { type Config } from 'tailwindcss'
+
+const config = {
+  content: ['./src/**/*.{tsx,mdx}'],
+  plugins: [memories],
+} satisfies Config
+
+export default config
