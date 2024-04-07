8821ce148bafd43f1a3a52bbf6cc003178adc378
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-04-07 22:32:15 +0900

    chore: Update package name in vite.config.ts

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'index',
+      name: '@inialum/memories-react',
       fileName: 'index',
       formats: ['es'],
     },
