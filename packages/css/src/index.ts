757c143bc56857a33e53370823008716383071c8
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-06-13 00:56:19 +0900

    feat: Add Navigation component to memories-css config

--- packages/css/src/index.ts
+++ packages/css/src/index.ts
@@ -1,13 +1,13 @@
 import plugin from 'tailwindcss/plugin'
 
-import { Button, ButtonLink, Footer } from '@/components'
+import { Button, ButtonLink, Footer, Navigation } from '@/components'
 import { tailwindConfig } from '@/tailwind/config'
 import { type ConfigOptions } from '@/types'
 
 const memories = plugin.withOptions<ConfigOptions>(
   (options) => {
     return ({ addComponents }) => {
-      addComponents([Button, ButtonLink, Footer])
+      addComponents([Button, ButtonLink, Footer, Navigation])
 
       if (options?.base !== false) {
         // TODO: Add base styles here
