5807bc017cbfb3fef84f42464e53728a851793dd
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-11-03 23:49:26 +0900

    chore: Update Storybook config

--- packages/css/.storybook/main.ts
+++ packages/css/.storybook/main.ts
@@ -1,25 +1,26 @@
-import { resolve } from 'node:path'
+import { dirname, join, resolve } from 'node:path'
 
 import type { StorybookConfig } from '@storybook/html-vite'
 
+// biome-ignore lint/suspicious/noExplicitAny: Suppressing this error because the type of `getAbsolutePath` is not explicitly defined in the Storybook API.
+const getAbsolutePath = (packageName: string): any =>
+	dirname(require.resolve(join(packageName, 'package.json')))
+
 const config: StorybookConfig = {
-	stories: [
-		'../stories/**/*.mdx',
-		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-	],
+	stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(ts|tsx)'],
+
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-a11y',
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-interactions'),
+		getAbsolutePath('@storybook/addon-a11y'),
 	],
+
 	framework: {
-		name: '@storybook/html-vite',
+		name: getAbsolutePath('@storybook/html-vite'),
 		options: {},
 	},
-	docs: {
-		autodocs: 'tag',
-	},
+
 	viteFinal(config) {
 		return {
 			...config,
